baa05b33e2e6a98f97528e107b2f7d4959141607
Matthew Malensek <matt@malensek.net>
2013-02-04 11:37:21 -0700

    Improve code readability

--- src/jbsdiff/Diff.java
+++ src/jbsdiff/Diff.java
@@ -68,53 +68,74 @@ public class Diff {
                 //System.out.println(result);
                 len = result.length;
 
-                for (;scsc<scan+len;scsc++) {
-                    if ((scsc+lastOffset<oldBytes.length) &&
-                            (oldBytes[scsc+lastOffset] == newBytes[scsc]))
+                for ( ; scsc < scan + len; scsc++) {
+                    if ((scsc + lastOffset < oldBytes.length) &&
+                            (oldBytes[scsc + lastOffset] == newBytes[scsc]))
                         oldScore++;
                 }
 
-                if (((len==oldScore) && (len!=0)) ||
-                        (len>oldScore+8)) break;
+                if (((len == oldScore) && (len != 0)) || (len > oldScore + 8)) {
+                    break;
+                }
 
-                if ((scan+lastOffset<oldBytes.length) &&
-                        (oldBytes[scan+lastOffset] == newBytes[scan]))
+                if ((scan + lastOffset < oldBytes.length) &&
+                        (oldBytes[scan + lastOffset] == newBytes[scan]))
                     oldScore--;
             }
 
-            if ((len!=oldScore) || (scan==newBytes.length)) {
-                s=0;Sf=0;lenf=0;
-                for (int i=0;(lastScan+i<scan)&&(lastPos+i<oldBytes.length);) {
-                    if (oldBytes[lastPos+i]==newBytes[lastScan+i]) s++;
+            if ((len != oldScore) || (scan == newBytes.length)) {
+                s = 0; Sf = 0; lenf = 0;
+                for (int i = 0; (lastScan + i < scan) && (lastPos + i < oldBytes.length); ) {
+                    if (oldBytes[lastPos + i] == newBytes[lastScan + i]) {
+                        s++;
+                    }
+
                     i++;
-                    if (s*2-i>Sf*2-lenf) { Sf=s; lenf=i; };
-                }
-
-                lenb=0;
-                if (scan<newBytes.length) {
-                    s=0;Sb=0;
-                    for (int i=1;(scan>=lastScan+i)&&(result.position>=i);i++) {
-                        if (oldBytes[result.position-i]==newBytes[scan-i]) s++;
-                        if (s*2-i>Sb*2-lenb) { Sb=s; lenb=i; };
+                    if (s * 2 - i > Sf * 2 - lenf) {
+                        Sf = s;
+                        lenf = i;
                     }
                 }
 
-                if (lastScan+lenf>scan-lenb) {
-                    overlap=(lastScan+lenf)-(scan-lenb);
-                    s=0;Ss=0;lens=0;
-                    for (int i=0;i<overlap;i++) {
-                        if (newBytes[lastScan+lenf-overlap+i]==
-                                oldBytes[lastPos+lenf-overlap+i]) s++;
-                        if (newBytes[scan-lenb+i]==
-                                oldBytes[result.position-lenb+i]) s--;
-                        if (s>Ss) { Ss=s; lens=i+1; };
+                lenb = 0;
+                if (scan < newBytes.length) {
+                    s = 0;
+                    Sb = 0;
+                    for (int i = 1; (scan >= lastScan + i) && (result.position >= i); i++) {
+                        if (oldBytes[result.position - i] == newBytes[scan - i]) {
+                            s++;
+                        }
+                        if (s * 2 - i > Sb * 2 - lenb) {
+                            Sb = s;
+                            lenb = i;
+                        }
                     }
+                }
 
+                if (lastScan + lenf > scan - lenb) {
+                    overlap = (lastScan + lenf) - (scan - lenb);
+                    s = 0;
+                    Ss = 0;
+                    lens = 0;
+                    for (int i=0; i < overlap; i++) {
+                        if (newBytes[lastScan + lenf - overlap + i] ==
+                                oldBytes[lastPos + lenf - overlap + i]) {
+                            s++;
+                        }
+                        if (newBytes[scan - lenb + i]==
+                                oldBytes[result.position - lenb + i]) {
+                            s--;
+                        }
+                        if (s > Ss) {
+                            Ss = s;
+                            lens = i + 1;
+                        }
+                    }
                     lenf += lens - overlap;
                     lenb -= lens;
-                };
+                }
 
-                for(int i=0;i<lenf;i++) {
+                for(int i = 0; i < lenf; i++) {
                     db[dblen + i] |= (newBytes[lastScan + i] -
                             oldBytes[lastPos + i]);
                 }
@@ -122,22 +143,21 @@ public class Diff {
                     eb[eblen + i] = newBytes[lastScan + lenf + i];
                 }
 
-                dblen+=lenf;
-                eblen+=(scan-lenb)-(lastScan+lenf);
+                dblen += lenf;
+                eblen += (scan - lenb) - (lastScan + lenf);
 
                 Offset.writeOffset(lenf, patchOut);
                 Offset.writeOffset((scan - lenb) - (lastScan + lenf), patchOut);
-                Offset.writeOffset((result.position - lenb) - (lastPos + lenf), patchOut);
+                Offset.writeOffset((result.position - lenb) -
+                        (lastPos + lenf), patchOut);
                 ctr++;
 
-                lastScan=scan-lenb;
-                lastPos=result.position-lenb;
-                lastOffset=result.position-scan;
+                lastScan = scan - lenb;
+                lastPos = result.position - lenb;
+                lastOffset = result.position - scan;
             }
         }
-        System.out.println(ctr);
         patchOut.close();
-        System.out.println(countOut.getCount());
 
         Header header = new Header();
         header.setControlLength(countOut.getCount());
@@ -153,32 +173,11 @@ public class Diff {
 
         header.setOutputLength(newBytes.length);
 
-        //header.write(headerOut);
-
         fOut.close();
 
         FileOutputStream f = new FileOutputStream(patchFile);
         header.write(f);
         f.write(fOut.toByteArray());
         f.close();
-        //int controlLength = 
-//        byte[] controlData = byteOut.toByteArray();
-//
-//        //Header header = new Header(controlData.length
-//        //
-//        ByteArrayOutputStream diffBytesOut = new ByteArrayOutputStream();
-//        CountingOutputStream cOut = new CountingOutputStream(diffBytesOut);
-//        BZip2CompressorOutputStream diffOut = new BZip2CompressorOutputStream(cOut);
-//        diffOut.write(db);
-//        diffOut.close();
-//        System.out.println(cOut.getCount());
-//        byte[] compressedDiffBlock = diffBytesOut.toByteArray();
-//        System.out.println(compressedDiffBlock.length);
-//
-//        pOut.write(compressedDiffBlock);
-//
-//        //header.write(pOut);
-//        pOut.write(controlData);
-//        pOut.close();
     }
 }
